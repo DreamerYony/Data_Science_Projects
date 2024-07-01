bbfc3fe7813ed3cdadc14e0a1d4814202230c9f5
DayeonLee <162097251+DreamerYony@users.noreply.github.com>
2024-07-01 20:26:37 +0900

    Update E_COMMERCE_SCHEMA.sql
    POSTGRESQL VERSION

--- E_COMMERCE/E_COMMERCE_SCHEMA.sql
+++ E_COMMERCE/E_COMMERCE_SCHEMA.sql
@@ -1,86 +1,134 @@
--- Exported from : dbdiagram.io
 -- Language : PostgreSQL
+-- 스키마
+-- 외래 키 설정은 맨 아래
 
-CREATE TABLE "customers" (
-  "Customer_id" varchar PRIMARY KEY,
-  "Customer_unique_id" varchar,
-  "Customer_zipcode_prefix" integer,
-  "Customer_city" varchar,
-  "Customer_state" varchar
+-- CUSTOMERS
+CREATE TABLE customers (
+    Customer_id VARCHAR(20) NOT NULL,
+    Customer_unique_id VARCHAR(30) NOT NULL,
+    Customer_zipcode_prefix INTEGER NOT NULL,
+    Customer_city VARCHAR(255) NOT NULL,
+    Customer_state VARCHAR(5) NOT NULL,
+    PRIMARY KEY (Customer_id)
 );
 
-CREATE TABLE "locations" (
-  "Geolocation_zipcode_prefix" integer,
-  "Geolocation_lat" float,
-  "Geolocation_lng" float,
-  "Geolocation_city" varchar,
-  "Geolocation_state" char,
-  PRIMARY KEY ("Geolocation_zipcode_prefix", "Geolocation_lat", "Geolocation_lng")
+SELECT * FROM customers;
+
+-- ORDER_ITEMS
+CREATE TABLE order_items (
+  Order_id VARCHAR(20),
+  Order_item_id VARCHAR(20),
+  Product_id VARCHAR(20),
+  Seller_id VARCHAR(20),
+  Price DECIMAL(10, 2),
+  Freight_value DECIMAL(10, 2),
+  PRIMARY KEY (order_id, order_item_id)
 );
 
-CREATE TABLE "order_items" (
-  "Order_id" varchar,
-  "Order_item_id" varchar,
-  "Product_id" varchar,
-  "Seller_id" varchar,
-  "Price" decimal,
-  "Freight_value" decimal,
-  PRIMARY KEY ("Order_id", "Order_item_id")
+SELECT * FROM order_items;
+
+-- ORDERS
+CREATE TABLE orders (
+  Order_id VARCHAR(20),
+  Customer_id VARCHAR(20),
+  Order_status VARCHAR(20),
+  Order_purchase_timestamp TIMESTAMP,
+  Order_delivered_carrier_date TIMESTAMP,
+  Order_delivered_customer_date TIMESTAMP,
+  Order_estimated_delivery_date DATE,
+  PRIMARY KEY (Order_id)
 );
 
-CREATE TABLE "orders" (
-  "Order_id" varchar PRIMARY KEY,
-  "Customer_id" varchar,
-  "Order_status" varchar,
-  "Order_purchase_timestamp" timestamp,
-  "Order_delivered_carrier_date" timestamp,
-  "Order_delivered_customer_date" timestamp,
-  "Order_estimated_delivery_date" timestamp
+SELECT * FROM ORDERS;
+
+-- PAYMENTS
+CREATE TABLE payments (	
+	Order_id VARCHAR(20),
+	Payment_sequential INTEGER,
+	Payment_type VARCHAR(20),
+	Payment_installments INTEGER,
+	Payment_value DECIMAL(10, 2),
+	PRIMARY KEY (Order_id, Payment_sequential)
+);
+ 
+SELECT * FROM payments;
+
+-- PRODUCTS
+-- 이상한 값 존재하는 상태 & 규격 컬럼들이 object 타입인 상태
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+    Product_id VARCHAR(30),	
+    Product_category_name VARCHAR(255),
+    Product_weight_g VARCHAR,
+    Product_length_cm VARCHAR,
+    Product_height_cm VARCHAR,
+    Product_width_cm VARCHAR,
+	PRIMARY KEY (Product_id)
 );
 
-CREATE TABLE "payments" (
-  "Order_id" varchar,
-  "Payment_sequential" integer,
-  "Payment_type" varchar,
-  "Payment_installments" integer,
-  "Payment_value" float,
-  PRIMARY KEY ("Order_id", "Payment_sequential")
+SELECT * FROM PRODUCTS;
+
+-- 이상한 값 알아보기
+SELECT * FROM products 
+WHERE NOT (Product_width_cm ~ '^[0-9]*\.?[0-9]+$') 
+   OR NOT (Product_height_cm ~ '^[0-9]*\.?[0-9]+$') 
+   OR NOT (Product_length_cm ~ '^[0-9]*\.?[0-9]+$')
+   OR NOT (Product_weight_g ~ '^[0-9]*\.?[0-9]+$');
+
+-- UNKNOWN 발견 : 해당 행은 NULL로 계산
+UPDATE products
+SET Product_width_cm = NULL
+WHERE NOT (Product_width_cm ~ '^[0-9]*\.?[0-9]+$');
+
+UPDATE products
+SET Product_height_cm = NULL
+WHERE NOT (Product_height_cm ~ '^[0-9]*\.?[0-9]+$');
+
+UPDATE products
+SET Product_length_cm = NULL
+WHERE NOT (Product_length_cm ~ '^[0-9]*\.?[0-9]+$');
+
+UPDATE products
+SET Product_weight_g = NULL
+WHERE NOT (Product_weight_g ~ '^[0-9]*\.?[0-9]+$');
+
+-- 수치 계산을 위해 FLOAT로 바꿔줌
+ALTER TABLE products
+ALTER COLUMN Product_width_cm TYPE FLOAT USING Product_width_cm::FLOAT,
+ALTER COLUMN Product_height_cm TYPE FLOAT USING Product_height_cm::FLOAT,
+ALTER COLUMN Product_length_cm TYPE FLOAT USING Product_height_cm::FLOAT,
+ALTER COLUMN Product_weight_g TYPE FLOAT USING Product_weight_g::FLOAT;
+
+-- REVIEWS
+CREATE TABLE reviews (
+	Review_id VARCHAR(20),
+	Order_id VARCHAR(20),
+	Review_score INTEGER,
+	Review_creation_date TIMESTAMP,
+	Review_answer_timestamp TIMESTAMP,
+	PRIMARY KEY (Review_id, Order_id)
 );
 
-CREATE TABLE "products" (
-  "Product_id" varchar,
-  "Product_item_id" varchar,
-  "Product_category_name" varchar,
-  "Product_weight_g" float,
-  "Product_length_cm" float,
-  "Product_height_cm" float,
-  "Product_width_cm" float,
-  PRIMARY KEY ("Product_id", "Product_item_id")
+SELECT * FROM REVIEWS;
+
+--SELLERS
+DROP TABLE IF EXISTS sellers CASCADE;
+CREATE TABLE sellers (
+    Seller_id VARCHAR(20) NOT NULL,
+    Seller_zipcode_prefix INTEGER NOT NULL,
+    Seller_city VARCHAR(255) NOT NULL,
+    Seller_state VARCHAR(5) NOT NULL,
+    PRIMARY KEY (Seller_id)
 );
 
-CREATE TABLE "reviews" (
-  "Review_id" varchar PRIMARY KEY,
-  "Order_id" varchar,
-  "Review_score" integer,
-  "Review_creation_date" timestamp,
-  "Review_answer_timestamp" timestamp
-);
+SELECT * FROM SELLERS;
 
-CREATE TABLE "sellers" (
-  "Seller_id" varchar PRIMARY KEY,
-  "Seller_zipcode_prefix" integer,
-  "Seller_city" varchar,
-  "Seller_state" char
-);
+-- 외래 키 설정
 
-ALTER TABLE "reviews" ADD FOREIGN KEY ("Order_id") REFERENCES "orders" ("Order_id");
+ALTER TABLE reviews ADD FOREIGN KEY (Order_id) REFERENCES orders (Order_id);
+ALTER TABLE payments ADD FOREIGN KEY (Order_id) REFERENCES orders (Order_id);
+ALTER TABLE order_items ADD FOREIGN KEY (Order_id) REFERENCES orders (Order_id);
+ALTER TABLE order_items ADD FOREIGN KEY (Seller_id) REFERENCES sellers (Seller_id);
+ALTER TABLE order_items ADD FOREIGN KEY (Product_id) REFERENCES products (Product_id);
+ALTER TABLE orders ADD FOREIGN KEY (Customer_id) REFERENCES customers(Customer_id);
 
-ALTER TABLE "order_items" ADD FOREIGN KEY ("Order_id") REFERENCES "orders" ("Order_id");
-
-ALTER TABLE "order_items" ADD FOREIGN KEY ("Seller_id") REFERENCES "sellers" ("Seller_id");
-
-ALTER TABLE "order_items" ADD FOREIGN KEY ("Product_id") REFERENCES "products" ("Product_id");
-
-ALTER TABLE "payments" ADD FOREIGN KEY ("Order_id") REFERENCES "orders" ("Order_id");
-
-ALTER TABLE "customers" ADD FOREIGN KEY ("Customer_id") REFERENCES "orders" ("Customer_id");
